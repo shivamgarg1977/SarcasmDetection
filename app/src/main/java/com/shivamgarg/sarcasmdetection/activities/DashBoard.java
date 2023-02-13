4d765eab175cea7fc3bf89b52ff6085f6c4cc944
Shivam Garg <shivamgarg1977@gmail.com>
2023-02-13 22:02:16 +0530

    navigation rail UI

--- app/src/main/java/com/shivamgarg/sarcasmdetection/activities/DashBoard.java
+++ app/src/main/java/com/shivamgarg/sarcasmdetection/activities/DashBoard.java
@@ -1,19 +1,22 @@
 package com.shivamgarg.sarcasmdetection.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.shivamgarg.sarcasmdetection.R;
 
 public class DashBoard extends AppCompatActivity {
 
+    CardView navigationBanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Log.d("Successfully Created","DashBoard: running");
+        Log.d("Successfully Created", "DashBoard: running");
+        navigationBanner = findViewById(R.id.navigation_banner);
 
 
     }
